9300c5068babcd83ff43f68e3f27181a8f818694
Tiger Soldier <tigersoldi@gmail.com>
2025-09-11 23:28:18 -0700

    fix: settings page not showing in Firefox
    Add browser_specific_settings to manifest.json to make the settings page work in Firefox.
    
    Also, migrate .eslintignore to eslint.config.js and format the code.

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,16 @@
-import globals from "globals";
-import js from "@eslint/js";
-import prettier from "eslint-config-prettier";
+import globals from 'globals';
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
 
 export default [
   js.configs.recommended,
   prettier,
   {
+    ignores: ['package-lock.json'],
     languageOptions: {
       globals: {
         ...globals.browser,
-        "chrome": "readonly"
+        chrome: 'readonly',
       },
     },
   },
